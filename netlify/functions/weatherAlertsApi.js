10649ff20158e47e6efed53615e34b4e30f89f00
kachmar-k <kkachmar3@gmail.com>
2024-04-24 09:59:03 -0400

    format response

--- netlify/functions/weatherAlertsApi.js
+++ netlify/functions/weatherAlertsApi.js
@@ -6,10 +6,16 @@ const handler = async function (event, context) {
   try {
     let apiUrl = `alerts/active/zone/${zoneId}`;
     const response = await fetchRequest(apiUrl, null, false);
-
+    let alertData = response.features[0];
+    console.log(JSON.stringify(alertData));
     return {
       statusCode: 200,
-      body: response,
+      body: {
+        title: alertData.title,
+        updated: alertData.updated,
+        alert: alertData.properties?.headline,
+        description: alertData.properties?.description,
+      },
     };
   } catch (error) {
     console.error(`Error with request.`, {
